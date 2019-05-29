0aa46b8bdc7d89f907783c2c42bb473db6685314
Arijit Basu <sayanarijit@gmail.com>
2019-05-29 09:41:48 +0530

    Drop Python 3.3 support
    Ref: https://github.com/sayanarijit/expandvars/issues/4

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
