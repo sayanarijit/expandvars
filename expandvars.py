729a265b4d619a99e04f53114142cadca2719661
Arijit Basu <sayanarijit@gmail.com>
2021-04-09 00:45:02 +0530

    Update version
    Also remove Python 2.7 support.

--- expandvars.py
+++ expandvars.py
@@ -8,7 +8,7 @@ __author__ = "Arijit Basu"
 __email__ = "sayanarijit@gmail.com"
 __homepage__ = "https://github.com/sayanarijit/expandvars"
 __description__ = "Expand system variables Unix style"
-__version__ = "v0.6.5"  # Also update pyproject.toml
+__version__ = "v0.7.0"  # Also update pyproject.toml
 __license__ = "MIT"
 __all__ = [
     "BadSubstitution",
